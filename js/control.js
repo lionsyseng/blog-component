15bdfbe5fe4e49a7de2bf1b61fe5d773e8850e37
lionsyseng <lionsyseng@gmail.com>
2026-03-20 15:22:27 +0100

    Segundo commit
    Se desarrolló la estructura HTML y la maquetación CSS de la tarjeta de visita del blog.

---
+++ js/control.js
@@ -0,0 +1,9 @@
+/**Funcion para mostrar acceso desde el botón */
+function button() {
+    onclick = alert("Ingresando desde el botón");
+}
+
+/**Funcion para mostrar acceso desde el título */
+function titulo() {
+    onclick = alert("Ingresando desde el título");
+}
