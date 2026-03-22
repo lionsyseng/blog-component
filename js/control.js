f1d4d4a304bdfdb532a99c3afbbe0090cb58b96d
lionsyseng <lionsyseng@gmail.com>
2026-03-22 15:41:15 +0100

    Quinto commit
    Ajuste de las propiedades del elemento button.

--- js/control.js
+++ js/control.js
@@ -1,11 +1,16 @@
 let mensaje = "HTML y CSS es la base de todas las páginas web. Define la estructura de una página, mientras que CSS define su estilo.";
 
-/**Funcion para mostrar acceso desde el botón */
+/** Funcion para mostrar mensaje de ingreso al blog */
 function button() {
     onclick = alert("Accediendo a Frontend Mentor");
 }
 
-/**Funcion para mostrar acceso desde el título */
+/** Funcion para mostrar acceso desde el título */
 function titulo() {
     onclick = alert(mensaje);
 }
+
+/** Función para direccionar al blog **/
+const acceso = document.getElementById("button").addEventListener("click", function() {
+    window.open("http://www.frontendmentor.io?ref=challenge", "_blank");
+});
