dfddc91abc4c1ce1a7cd455b69ce57be231f5b32
lionsyseng <lionsyseng@gmail.com>
2026-03-22 13:49:42 +0100

    Tercer commit
    Subida del código de la solución.

--- js/control.js
+++ js/control.js
@@ -1,9 +1,11 @@
+let mensaje = "HTML y CSS es la base de todas las páginas web. Define la estructura de una página, mientras que CSS define su estilo.";
+
 /**Funcion para mostrar acceso desde el botón */
 function button() {
-    onclick = alert("Ingresando desde el botón");
+    onclick = alert("Accediendo a Frontend Mentor");
 }
 
 /**Funcion para mostrar acceso desde el título */
 function titulo() {
-    onclick = alert("Ingresando desde el título");
+    onclick = alert(mensaje);
 }
